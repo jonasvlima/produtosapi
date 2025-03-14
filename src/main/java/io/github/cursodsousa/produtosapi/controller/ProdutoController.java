e022076191f40b68e8f35dbcbd25827134150831
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-03-13 21:09:00 -0300

    feat: add search with parameters

--- src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import io.github.cursodsousa.produtosapi.model.Produto;
 import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -41,5 +42,10 @@ public class ProdutoController {
                           @RequestBody Produto produto) {
         produto.setId(id);
         produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome) {
+        return produtoRepository.findByNome(nome);
     }
 }
